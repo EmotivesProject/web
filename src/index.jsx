34c4a58306e5d93776f7cb4b45d08cc2e1c33a07
Tom Bowyer <tom4310@gmail.com>
2021-03-16 07:47:49 +1000

    Added redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 // import reportWebVitals from './reportWebVitals';
 
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  rootElement,
 );
 
 // reportWebVitals(console.log);
---
+++ src/redux/actionTypes.js
@@ -0,0 +1,2 @@
+export const SET_FILTER = 'SET_FILTER';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
