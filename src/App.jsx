535f6377de6c1807bdfb978a6469b307e7a657cc
Tom Bowyer <tom4310@gmail.com>
2021-05-25 06:54:56 +1000

    Major rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/Register';
-import Error from './pages/Error';
-import LogIn from './pages/LogIn';
-import Messenger from './pages/Messenger';
-import './site.css';
 
 function App() {
   return (
-    <main>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/register" component={SignUp} />
-        <Route path="/login" component={LogIn} />
-        <Route path="/messenger" component={Messenger} />
-        <Route component={Error} />
-      </Switch>
-    </main>
+    <p>
+      Hello
+    </p>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
 import App from './App';
-import 'semantic-ui-css/semantic.min.css';
-// import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  rootElement,
+  <App />,
+  document.getElementById('root'),
 );
-
-// reportWebVitals(console.log);
