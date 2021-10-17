e7d17eacad94f6c92a49b6970bb74fed53f85f76
Tom Bowyer <tom4310@gmail.com>
2021-10-17 10:31:19 +1000

    Able to set a profile picture

---
+++ src/profile/PictureComponent.jsx
@@ -0,0 +1,85 @@
+import axios from 'axios';
+import React from 'react';
+import {
+  Button,
+  Form,
+  Grid, Image, Message, Segment,
+} from 'semantic-ui-react';
+import createProfileLink from '../utils/createProfileLink';
+import { extractErrorObject } from '../utils/extractObjects';
+
+const host = process.env.REACT_APP_API_HOST;
+const base = process.env.REACT_APP_MEDIA_UPLOAD_BASE_URL;
+
+export const PictureComponent = ({ auth }) => {
+  const [error, setError] = React.useState(null);
+  const [newFile, SetNewFile] = React.useState([]);
+
+  const errorMessage = error !== null ? (
+    <Message negative>
+      <Message.Header>Error occurred</Message.Header>
+      <p>
+        {error}
+        &nbsp;try again or refresh
+      </p>
+    </Message>
+  ) : null;
+
+  const fileChange = (event) => {
+    SetNewFile(event.target.files[0]);
+  };
+
+  const onFormSubmit = () => {
+    const url = `${host}://${base}/user_profile`;
+    const formData = new FormData();
+    formData.append('image', newFile);
+    axios.post(
+      url,
+      formData,
+      {
+        headers: {
+          'content-type': 'multipart/form-data',
+          Authorization: `Bearer ${auth.token}`,
+        },
+      },
+    ).then(() => {
+      setError(null);
+      window.location.reload(false);
+    }).catch((err) => {
+      setError(extractErrorObject(err).message);
+    });
+  };
+
+  return (
+    <Segment>
+      {errorMessage}
+      <h2>
+        Profile Picture
+      </h2>
+      <Grid columns={2} textAlign="center">
+        <Grid.Column>
+          <h3>Current</h3>
+          <Image
+            src={createProfileLink(auth.username)}
+            size="medium"
+            className="profile-display"
+          />
+        </Grid.Column>
+        <Grid.Column>
+          <h3>New</h3>
+          <Form onSubmit={onFormSubmit}>
+            <Form.Field>
+              <Button as="label" htmlFor="file" type="button">
+                Select a new profile picture
+              </Button>
+              <input type="file" id="file" hidden onChange={fileChange} />
+            </Form.Field>
+            <Button type="submit">Confirm</Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    </Segment>
+  );
+};
+
+export default PictureComponent;
---
+++ src/profile/ProfilePage.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import { Grid, Message } from 'semantic-ui-react';
+import getAuth from '../auth/selector';
+import TopBar from '../shared/TopBar';
+import { PictureComponent } from './PictureComponent';
+
+export const ProfilePage = ({
+  auth,
+}) => {
+  if (auth === null) {
+    return <Redirect to="/" />;
+  }
+
+  const [error] = React.useState(null);
+
+  const errorMessage = error !== null ? (
+    <Message negative>
+      <Message.Header>Error occurred</Message.Header>
+      <p>
+        An error occurred around&nbsp;
+        {error}
+        &nbsp;try again or refresh
+      </p>
+    </Message>
+  ) : null;
+
+  return (
+    <>
+      <TopBar />
+      <Grid role="main" id="main">
+        <Grid.Row columns="three">
+          <Grid.Column />
+          <Grid.Column>
+            {errorMessage}
+            <h1>Profile</h1>
+            <PictureComponent auth={auth} />
+          </Grid.Column>
+          <Grid.Column />
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  auth: getAuth(state),
+});
+
+export default connect(mapStateToProps, null)(ProfilePage);
